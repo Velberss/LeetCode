2cf96e6a569060f4bfbd43cda8a02ecc0521365f
Velbers <goncalvesvelber@gmail.com>
2025-07-01 20:09:50 -0300

    ++

---
+++ 3330. Find the Original Typed String I/main.java
@@ -0,0 +1,25 @@
+package abc;
+
+
+class Solution {
+    public int possibleStringCount(String word) {
+        int count = 1; // sempre existe pelo menos 1 forma (sem erro)
+        int i = 0;
+
+        while (i < word.length()) {
+            int j = i;
+            while (j < word.length() && word.charAt(j) == word.charAt(i)) {
+                j++;
+            }
+            int groupLength = j - i;
+            if (groupLength > 1) {
+                count += groupLength - 1; // possibilidades de erro nesse grupo
+            }
+            i = j;
+        }
+
+        return count;
+    }
+}
+
+
